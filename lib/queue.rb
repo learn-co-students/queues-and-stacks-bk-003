96c26bd32ffab8730588729a3e4d15d58e9d51a5
github username <mendelblesofsky@gmail.com>
2016-01-05 15:40:50 -0500

    clanky, may as well just use good ole shift

--- lib/queue.rb
+++ lib/queue.rb
@@ -2,4 +2,22 @@ require_relative 'stack'
 
 class Queue
 
+	attr_accessor :stack1, :stack2
+
+	def initialize
+		@stack1 = Stack.new
+		@stack2 = Stack.new
+	end
+
+	def enqueue(item)
+		stack1.push(item)
+	end
+
+	def dequeue
+		stack1.size.times { |i| stack2.push(stack1.pop) }
+		elem = stack2.pop
+		stack2.size.times { |i| stack1.push(stack2.pop) }
+		elem
+	end
+
 end
--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,20 @@
 class Stack
+	attr_accessor :data
+	
+	def initialize
+		@data = []
+	end
+
+	def push(item)
+		data.push(item)
+	end
+
+	def pop
+		data.pop
+	end
+
+	def size
+		data.size
+	end
 
 end
