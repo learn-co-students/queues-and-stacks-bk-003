58029a9b056412d7ebae86012446be417df13316
leonardo <l.castillo1989@Hotmail.com>
2016-06-20 03:58:53 -0400

    dope

--- lib/queue.rb
+++ lib/queue.rb
@@ -2,4 +2,28 @@ require_relative 'stack'
 
 class Queue
 
+	def initialize
+    @stack1 = Stack.new
+    @stack2 = Stack.new
+  end
+
+  def enqueue num
+  	@stack1.push num
+  end
+
+  def dequeue
+    @stack1.size.times do |i|
+      element = @stack1.pop
+      @stack2.push element
+    end
+    result = @stack2.pop
+    @stack2.size.times do |i|
+    	element = @stack2.pop
+    	@stack1.push element
+    end
+    result
+  end
+
+
+
 end
--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,23 @@
+require "pry"
 class Stack
+  attr_accessor :datastore, :size
+
+  def initialize
+    @datastore = []
+    @size = 0
+  end
+  
+  def push element
+    @datastore.push(element)
+    @size += 1
+  end
+
+  def pop 
+    @size -= 1
+    @datastore.pop 
+  end
 
 end
+
+
+
